7e03855c9020eb7b4a1264d3603255e9cd9567b1
pinokio <pinokio>
2025-12-30 20:51:12 -0500

    Update install.js

--- install.js
+++ install.js
@@ -3,6 +3,24 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        message: [
+          "git clone https://github.com/alexta69/metube app"
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        path: "app/ui",
+        message: [
+          "pnpm install",
+          "pnpm run build"
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
         venv: "env",
         venv_python: "3.13",
         path: "app",
